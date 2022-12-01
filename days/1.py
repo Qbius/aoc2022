4ecedf44a05e84bb2cbdb929682fe3f41dfe32d3
qbius <my@mail.com>
2022-12-01 14:17:21 +0100

    typo

--- days/1.py
+++ days/1.py
@@ -1,11 +1,11 @@
 from common import raw_input
 
-def get_elfs(rawinpt):
+def get_elves(rawinpt):
     return [list(map(int, elf.split('\n'))) for elf in rawinpt.split('\n\n')]
 
 def first(rawinpt: raw_input):
-    return max(map(sum, get_elfs(rawinpt)))
+    return max(map(sum, get_elves(rawinpt)))
 
 def second(rawinpt: raw_input):
-    top1, top2, top3, *_rest = sorted(map(sum, get_elfs(rawinpt)))[::-1]
+    top1, top2, top3, *_rest = sorted(map(sum, get_elves(rawinpt)))[::-1]
     return top1 + top2 + top3
