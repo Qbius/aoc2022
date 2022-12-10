94c2df4ce3d4ac62d22beaf210f1435d8229a1b8
qbius <theqbius@gmail.com>
2022-12-10 12:21:22 +0100

    day 10

---
+++ days/10.py
@@ -0,0 +1,157 @@
+from pandas import Series
+
+def parse(rawinpt):
+    return Series([1, *list(map(lambda l: 0 if l == 'noop' else int(l), [line.split(' ')[-1] for line in rawinpt.replace('addx', 'noop\naddx').split('\n')]))]).cumsum()
+
+def first(xregister):
+    return sum(xregister[cycle - 1] * cycle for cycle in [20, 60, 100, 140, 180, 220])
+
+def second(xregister):
+    print(''.join(('\n' if i > 0 and i % 40 == 0 else '') + ('#' if s else '.') for i, s in enumerate((Series(range(len(xregister))) % 40 - xregister).abs() <= 1)))
+
+example = '''addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop'''
