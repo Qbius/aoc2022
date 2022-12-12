0130089ff0bb4e9d81b1c2d937d54874707ca8e1
qbius <theqbius@gmail.com>
2022-12-12 17:29:49 +0100

    day 12

--- days/12.py
+++ days/12.py
@@ -4,20 +4,20 @@ def parse(rawinpt):
     end = next((x, y) for x in range(len(grid[0])) for y in range(len(grid)) if grid[y][x] == 'E')
     return start, end, [list(line) for line in rawinpt.replace('S', 'a').replace('E', 'z').split('\n')]
 
-def first(start, end, grid):
+def djikstra(start, endpoints, grid):
     width = len(grid[0])
     height = len(grid)
     max_distance = width * height
     distances = {(x, y): max_distance for x in range(width) for y in range(height)}
     distances[start] = 0
     visited = set()
-    while end not in visited:
+    while any(point not in visited for point in endpoints):
         current = min([point for point in distances.keys() if point not in visited], key=distances.get)
         cx, cy = current
 
         not_visited = lambda x, y: (x, y) not in visited
         in_bounds = lambda x, y: x >= 0 and x < width and y >= 0 and y < height
-        ok_label = lambda x, y: (ord(grid[y][x]) - ord(grid[cy][cx])) in [0, 1]
+        ok_label = lambda x, y: (ord(grid[cy][cx]) - ord(grid[y][x])) <= 1
         
         nbrs = [(cx - 1, cy), (cx + 1, cy), (cx, cy - 1), (cx, cy + 1)]
         nbrs = [point for point in nbrs if not_visited(*point) and in_bounds(*point) and ok_label(*point)]
@@ -25,10 +25,14 @@ def first(start, end, grid):
         for point in nbrs:
             distances[point] = distances[current] + 1
         visited.add(current)
-    return distances[end]
+    return min(map(distances.get, endpoints))
 
-def second(start, end, grid):
-    pass
+def first(start, end, grid):
+    return djikstra(end, [start], grid)
+
+def second(_start, end, grid):
+    ass = [(x, y) for x in range(len(grid[0])) for y in range(len(grid)) if grid[y][x] == 'a']
+    return djikstra(end, ass, grid)
 
 example = '''Sabqponm
 abcryxxl
